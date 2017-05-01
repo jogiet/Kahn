2703b5ed3994024d99a76cc8aeeb7825c4031a4f
Alexis Thibault <alexis.thibault1@gmail.com>
2017-05-01 18:00:20 +0200

    Fix network_try et ajout dans le Makefile

--- src/network_try.ml
+++ src/network_try.ml
@@ -4,29 +4,34 @@ module B = Bytes
 
 (* fichier serveur *)
 
+let my_print_string s =
+  print_string s; flush_all ()
 
 let () =
-	let s = U.socket U.PF_INET U.SOCK_STREAM 0 in
-	let ip_addr = U.inet_addr_any
-	and port = 12345 in
-	let addr = U.ADDR_INET (ip_addr,port)
-	in begin
-		U.bind s addr;
-		U.listen s 20;
-		let s_cl , addr_cl = U.accept s in
-		let header = B.create M.header_size 
-		in begin
-			ignore (U.read s_cl header 0 M.header_size);
-			let size = M.data_size header 0 in
-			let data = B.create size 
-			in begin
-				ignore (U.read s_cl data 0 size);
-				let res =  M.from_bytes (B.cat header data) 0
-				in print_string res;
-				U.shutdown s_cl U.SHUTDOWN_ALL;
-			end;
-		end;
-	end;
+  let ip_addr = U.inet_addr_any
+  and port = 12345 in
+  let addr = U.ADDR_INET (ip_addr,port) in
+  let domain = Unix.domain_of_sockaddr addr in
+  let s = U.socket domain U.SOCK_STREAM 0 in
+  U.bind s addr;
+  U.listen s 20;
+  my_print_string "S:Created. Accepting clients...\n";
+  let s_cl , addr_cl = U.accept s in
+  my_print_string "S:Client connected.\n";
+  let header = B.create M.header_size 
+  in
+  ignore (U.read s_cl header 0 M.header_size);
+  let size = M.data_size header 0 in
+  let data = B.create size 
+  in
+  my_print_string "S:Reading data.\n";
+  ignore (U.read s_cl data 0 size);
+  let res =  M.from_bytes (B.cat header data) 0
+  in
+  print_string "S:The message is : ";
+  print_string res;
+  my_print_string "S:How nice. Exiting.\n";
+  U.shutdown s_cl U.SHUTDOWN_ALL;
 
 
 
--- src/network_try_client.ml
+++ src/network_try_client.ml
@@ -3,18 +3,23 @@ module U = Unix
 module M = Marshal
 
 let () = 
-	let s = U.socket U.PF_INET U.SOCK_STREAM 0 in
-	let host_name = "sas" in
-	let host = U.gethostbyname host_name in
-	let ip_addr = host.h_addr_list.(0) in
-	let port = 12345 in
-	let addr = U.ADDR_INET (ip_addr,port)
-	in begin
-		U.connect s addr;
-		let bycode = M.to_bytes "Hello World !\n" [] in
-			ignore (U.write s bycode 0 (B.length bycode));
-		U.shutdown s U.SHUTDOWN_ALL;
-	end
+  let s = U.socket U.PF_INET U.SOCK_STREAM 0 in
+  let host_name = "127.0.0.1" in
+  let host = U.gethostbyname host_name in
+  let ip_addr = host.U.h_addr_list.(0) in
+  let port = 12345 in
+  let addr = U.ADDR_INET (ip_addr,port)
+  in begin
+    print_string "C:Waiting a second to ensure server is alive...\n";
+    flush_all ();
+    U.sleep 1;
+    U.connect s addr;
+    print_string "C:Connected. Sending bytes...\n";
+    let bycode = M.to_bytes "Hello, you are my favorite server!\n" [] in
+    ignore (U.write s bycode 0 (B.length bycode));
+    print_string "C:Success!\n";
+    U.shutdown s U.SHUTDOWN_ALL;
+  end
 
 
 
